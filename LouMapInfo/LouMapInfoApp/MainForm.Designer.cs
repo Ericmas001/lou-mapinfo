3f29340337f9cc1b91ab89c0fc10a72c6accd048
Éric Massé <Ericmas001@users.noreply.github.com>
2010-11-28 22:55:17 +0000

    Windows App

---
+++ LouMapInfo/LouMapInfoApp/MainForm.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace LouMapInfoApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.AllianceName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.AllianceScore = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PlayerName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PlayerScore = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CityX = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CityY = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CityName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CityCastle = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.CityScore = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(683, 448);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(675, 422);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Cities";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.AllianceName,
+            this.AllianceScore,
+            this.PlayerName,
+            this.PlayerScore,
+            this.CityX,
+            this.CityY,
+            this.CityName,
+            this.CityCastle,
+            this.CityScore});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dataGridView1.Size = new System.Drawing.Size(669, 416);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // AllianceName
+            // 
+            this.AllianceName.HeaderText = "Alliance Name";
+            this.AllianceName.Name = "AllianceName";
+            this.AllianceName.ReadOnly = true;
+            // 
+            // AllianceScore
+            // 
+            this.AllianceScore.HeaderText = "Alliance Score";
+            this.AllianceScore.Name = "AllianceScore";
+            this.AllianceScore.ReadOnly = true;
+            // 
+            // PlayerName
+            // 
+            this.PlayerName.HeaderText = "PlayerName";
+            this.PlayerName.Name = "PlayerName";
+            this.PlayerName.ReadOnly = true;
+            // 
+            // PlayerScore
+            // 
+            this.PlayerScore.HeaderText = "PlayerScore";
+            this.PlayerScore.Name = "PlayerScore";
+            this.PlayerScore.ReadOnly = true;
+            // 
+            // CityX
+            // 
+            this.CityX.HeaderText = "X";
+            this.CityX.Name = "CityX";
+            this.CityX.ReadOnly = true;
+            // 
+            // CityY
+            // 
+            this.CityY.HeaderText = "Y";
+            this.CityY.Name = "CityY";
+            this.CityY.ReadOnly = true;
+            // 
+            // CityName
+            // 
+            this.CityName.HeaderText = "Name";
+            this.CityName.Name = "CityName";
+            this.CityName.ReadOnly = true;
+            // 
+            // CityCastle
+            // 
+            this.CityCastle.HeaderText = "Castle";
+            this.CityCastle.Name = "CityCastle";
+            this.CityCastle.ReadOnly = true;
+            // 
+            // CityScore
+            // 
+            this.CityScore.HeaderText = "Score";
+            this.CityScore.Name = "CityScore";
+            this.CityScore.ReadOnly = true;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 451);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(683, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(270, 17);
+            this.toolStripStatusLabel1.Text = "Word: 10, Continent: 41, Last Updated: 2010-11-28";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(683, 473);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LoU Map Info";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn AllianceName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn AllianceScore;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PlayerName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PlayerScore;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CityX;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CityY;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CityName;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn CityCastle;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CityScore;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ LouMapInfo/LouMapInfoApp/MainForm.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using LouMapInfo.Entities;
+using System.IO;
+using EricUtility.Networking.JSON;
+
+namespace LouMapInfoApp
+{
+    public partial class MainForm : Form
+    {
+        private static Dictionary<int, WorldInfo> worlds = new Dictionary<int, WorldInfo>();
+        public static void loadUpdated()
+        {
+            string s = File.ReadAllText("../../testData/updated.json");
+            JsonTextParser parser = new JsonTextParser();
+            JsonObject jsonObject = parser.Parse(s);
+
+            foreach (JsonObject field in jsonObject as JsonObjectCollection)
+            {
+                int worldID = int.Parse(field.Name);
+                DateTime val = DateTime.Parse((string)field.GetValue());
+                worlds.Add(worldID, new WorldInfo(worldID, val));
+            }
+        }
+
+
+        public static void loadShrines()
+        {
+            string s = File.ReadAllText("../../testData/sea10.json");
+            JsonTextParser parser = new JsonTextParser();
+            JsonObject jsonObject = parser.Parse(s);
+            WorldInfo w = worlds[10];
+            foreach (JsonObject o in jsonObject as JsonObjectCollection)
+            {
+                JsonObjectCollection t = o as JsonObjectCollection;
+                if (t != null)
+                {
+                    foreach (JsonArrayCollection c in t)
+                    {
+                        int cid = int.Parse(c.Name);
+                        switch (t.Name)
+                        {
+                            case "shrines":
+                                {
+                                    foreach (JsonArrayCollection i in c)
+                                    {
+                                        w.Cont(cid).Shrines.Add(new Pt((int)((JsonNumericValue)i[0]).Value, (int)((JsonNumericValue)i[1]).Value));
+                                    }
+                                    break;
+                                }
+                            case "moongates":
+                                {
+                                    foreach (JsonArrayCollection i in c)
+                                    {
+                                        w.Cont(cid).MoonGates.Add(new Pt((int)((JsonNumericValue)i[0]).Value, (int)((JsonNumericValue)i[1]).Value));
+                                    }
+                                    break;
+                                }
+                        }
+
+                    }
+                }
+            }
+
+        }
+
+
+        public static void loadOverlay()
+        {
+            string s = File.ReadAllText("../../testData/overlay10c41.json");
+            JsonTextParser parser = new JsonTextParser();
+            JsonObject jsonObject = parser.Parse(s);
+            WorldInfo world = worlds[10];
+            ContinentInfo continent = world.Cont(41);
+            foreach (JsonObjectCollection a in jsonObject as JsonObjectCollection)
+            {
+                string aid = a.Name;
+                int pts = (int)((JsonNumericValue)a["points"]).Value;
+                string name = (string)((JsonStringValue)a["name"]).Value;
+                AllianceInfo alliance = new AllianceInfo(int.Parse(aid), name, pts, continent);
+                if (continent.Alliances.ContainsKey(name))
+                    alliance = continent.Alliances[name];
+                else
+                    continent.Alliances.Add(name, alliance);
+                JsonObjectCollection players = (JsonObjectCollection)a["players"];
+                foreach (JsonObjectCollection p in players)
+                {
+                    string pid = p.Name;
+                    string pname = (string)((JsonStringValue)p["name"]).Value;
+                    int ppts = (int)((JsonNumericValue)p["points"]).Value;
+                    PlayerInfo player = new PlayerInfo(int.Parse(pid), pname, ppts, alliance);
+                    if (alliance.Players.ContainsKey(pname))
+                        player = alliance.Players[pname];
+                    else
+                        alliance.Players.Add(pname, player);
+                    JsonObjectCollection pcities = (JsonObjectCollection)p["cities"];
+                    foreach (JsonObjectCollection c in pcities)
+                    {
+                        string cid = c.Name;
+                        string cname = (string)((JsonStringValue)c["name"]).Value;
+                        int cpts = (int)((JsonNumericValue)c["points"]).Value;
+                        bool castle = (bool)((JsonBooleanValue)c["castle"]).Value;
+                        Pt loc = new Pt((int)((JsonNumericValue)c["x"]).Value, (int)((JsonNumericValue)c["y"]).Value);
+                        CityInfo city = new CityInfo(int.Parse(cid), cname, cpts, castle, loc, player);
+                        player.Cities.Add(cid, city);
+                        if (castle)
+                            Console.WriteLine(alliance + ": " + player + ": " + city);
+                    }
+                }
+            }
+        }
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            loadUpdated();
+            loadShrines();
+            loadOverlay();
+            foreach (AllianceInfo a in worlds[10].Cont(41).Alliances.Values)
+            {
+
+                foreach (PlayerInfo p in a.Players.Values)
+                {
+
+                    foreach (CityInfo c in p.Cities.Values)
+                    {
+                        dataGridView1.Rows.Add(a.Name, a.Score, p.Name, p.Score, c.Location.X, c.Location.Y, c.Name, c.Castle, c.Score);
+                    }
+
+                }
+            }
+        }
+    }
+}
