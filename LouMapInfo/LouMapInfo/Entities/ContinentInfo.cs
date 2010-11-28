2b566703d7d22e550348e578dd4f76e31e37a8c4
Éric Massé <Ericmas001@users.noreply.github.com>
2010-11-28 20:28:39 +0000

    Shrines & MoonGates

---
+++ LouMapInfo/LouMapInfo/Entities/ContinentInfo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LouMapInfo.Entities
+{
+    public class ContinentInfo : IComparable<ContinentInfo>
+    {
+        private readonly int m_ID;
+        private readonly WorldInfo m_World;
+        private readonly List<Pt> m_Shrines = new List<Pt>();
+        private readonly List<Pt> m_MoonGates = new List<Pt>();
+
+        public int ID { get { return m_ID; } }
+        public WorldInfo World { get { return m_World; } }
+        public List<Pt> Shrines { get { return m_Shrines; } }
+        public List<Pt> MoonGates { get { return m_MoonGates; } }
+
+        public ContinentInfo(int id, WorldInfo parent)
+        {
+            m_ID = id;
+            m_World = parent;
+        }
+
+        #region IComparable<ContinentInfo> Members
+
+        public int CompareTo(ContinentInfo other)
+        {
+            return m_ID.CompareTo(other.ID);
+        }
+
+        #endregion
+    }
+}
--- LouMapInfo/LouMapInfo/Program.cs
+++ LouMapInfo/LouMapInfo/Program.cs
@@ -27,15 +27,78 @@ namespace LouMapInfo
             {
                 int worldID = int.Parse(field.Name);
                 DateTime val = DateTime.Parse((string)field.GetValue());
-                worlds.Add(worldID,new WorldInfo(worldID, val));
+                worlds.Add(worldID, new WorldInfo(worldID, val));
+            }
+        }
+
+        
+        public static void loadShrines()
+        {
+            string s = File.ReadAllText("../../testData/sea10.json");
+            JsonTextParser parser = new JsonTextParser();
+            JsonObject jsonObject = parser.Parse(s);
+
+            //Console.WriteLine();
+            //Console.WriteLine("loadShrines() indentation in JSON data format:");
+            //Console.WriteLine(jsonObject.ToString());
+            //Console.WriteLine();
+            WorldInfo w = worlds[10];
+            List<int> cont = new List<int>();
+            foreach (JsonObject o in jsonObject as JsonObjectCollection)
+            {
+                JsonObjectCollection t = o as JsonObjectCollection;
+                if (t != null)
+                {
+                    foreach (JsonArrayCollection c in t)
+                    {
+                        int cid = int.Parse(c.Name);
+                        switch (t.Name)
+                        {
+                            case "shrines":
+                                {
+                                    foreach (JsonArrayCollection i in c)
+                                    {
+                                        w.Cont(cid).Shrines.Add(new Pt((int)((JsonNumericValue)i[0]).Value, (int)((JsonNumericValue)i[1]).Value));
+                                    }
+                                    break;
+                                }
+                            case "moongates":
+                                {
+                                    foreach (JsonArrayCollection i in c)
+                                    {
+                                        w.Cont(cid).MoonGates.Add(new Pt((int)((JsonNumericValue)i[0]).Value, (int)((JsonNumericValue)i[1]).Value));
+                                    }
+                                    break;
+                                }
+                        }
+
+                    }
+                }
+                /*
+                int worldID = int.Parse(field.Name);
+                DateTime val = DateTime.Parse((string)field.GetValue());
+                worlds.Add(worldID,new WorldInfo(worldID, val));*/
             }
 
         }
         static void Main(string[] args)
         {
             loadUpdated();
-            foreach (WorldInfo wi in worlds.Values)
-                Console.WriteLine("World #{0}, last updated {1:yyyy}-{1:mm}-{1:dd}", wi.ID, wi.LastUpdated);
+            //foreach (WorldInfo wi in worlds.Values)
+            //    Console.WriteLine("World #{0}, last updated {1:yyyy}-{1:mm}-{1:dd}", wi.ID, wi.LastUpdated);
+            //Console.ReadLine();
+            loadShrines();
+            foreach (ContinentInfo c in worlds[10].Continents)
+            {
+                foreach (Pt p in worlds[10].Cont(c.ID).Shrines)
+                {
+                    Console.WriteLine("Shrine   World " + c.World.ID + " Cont " + c.ID + ": " + p);
+                }
+                foreach (Pt p in worlds[10].Cont(41).MoonGates)
+                {
+                    Console.WriteLine("MoonGate World " + c.World.ID + " Cont " + c.ID + ": " + p);
+                }
+            }
             Console.ReadLine();
         }
     }
