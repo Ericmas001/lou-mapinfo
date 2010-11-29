2199fdcfaf37d940a0deb3a8941e37b349ccbe30
Éric Massé <Ericmas001@users.noreply.github.com>
2010-11-29 00:57:00 +0000

    Lawless reports

---
+++ LouMapInfo/LouMapInfoLib/Entities/Pt.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LouMapInfo.Entities
+{
+    public class Pt
+    {
+        private int m_x;
+
+        public int X
+        {
+            get { return m_x; }
+            set { m_x = value; }
+        }
+        private int m_y;
+
+        public int Y
+        {
+            get { return m_y; }
+            set { m_y = value; }
+        }
+        public Pt()
+        {
+        }
+        public Pt(int x, int y)
+        {
+            m_x = x;
+            m_y = y;
+        }
+        public override string ToString()
+        {
+            return m_x + ":" + m_y;
+        }
+    }
+}
