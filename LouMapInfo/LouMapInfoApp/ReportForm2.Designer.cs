849ae18291cfc61e9e85e8ee9e4740b04717bf32
Éric Massé <Ericmas001@users.noreply.github.com>
2010-12-01 16:56:56 +0000

    White BBCode

--- LouMapInfo/LouMapInfoApp/ReportForm2.Designer.cs
+++ LouMapInfo/LouMapInfoApp/ReportForm2.Designer.cs
@@ -171,6 +171,7 @@
             // 
             // txtBBCode
             // 
+            this.txtBBCode.BackColor = System.Drawing.Color.White;
             this.txtBBCode.Dock = System.Windows.Forms.DockStyle.Fill;
             this.txtBBCode.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtBBCode.Location = new System.Drawing.Point(3, 28);
