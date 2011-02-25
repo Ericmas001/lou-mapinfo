f76704ff24d27e8c2b7b4759bd8f004fdde45201
Éric Massé <Ericmas001@users.noreply.github.com>
2011-02-25 21:52:14 +0000

    ContinentOverview V4 ! V4 is now working as the 3.1 version

--- LouMapInfo/LouMapInfoApp/V4/LouOfficial/ContentLouContinent.Designer.cs
+++ LouMapInfo/LouMapInfoApp/V4/LouOfficial/ContentLouContinent.Designer.cs
@@ -28,7 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ContentLouContinent));
+            this.tbReportContinentOverview = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel8 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripLabel9 = new System.Windows.Forms.ToolStripLabel();
+            this.pnlContent = new System.Windows.Forms.Panel();
+            this.btnChooseContinent = new System.Windows.Forms.ToolStripButton();
+            this.tbReportContinentOverview.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tbReportContinentOverview
+            // 
+            this.tbReportContinentOverview.BackColor = System.Drawing.Color.White;
+            this.tbReportContinentOverview.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.tbReportContinentOverview.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel8,
+            this.toolStripSeparator4,
+            this.toolStripLabel9,
+            this.btnChooseContinent});
+            this.tbReportContinentOverview.Location = new System.Drawing.Point(0, 0);
+            this.tbReportContinentOverview.Name = "tbReportContinentOverview";
+            this.tbReportContinentOverview.Size = new System.Drawing.Size(435, 25);
+            this.tbReportContinentOverview.TabIndex = 18;
+            this.tbReportContinentOverview.Text = "toolStrip2";
+            // 
+            // toolStripLabel8
+            // 
+            this.toolStripLabel8.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.toolStripLabel8.Name = "toolStripLabel8";
+            this.toolStripLabel8.Size = new System.Drawing.Size(126, 22);
+            this.toolStripLabel8.Text = "Continent Overview: ";
+            // 
+            // toolStripSeparator4
+            // 
+            this.toolStripSeparator4.Name = "toolStripSeparator4";
+            this.toolStripSeparator4.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripLabel9
+            // 
+            this.toolStripLabel9.Name = "toolStripLabel9";
+            this.toolStripLabel9.Size = new System.Drawing.Size(40, 22);
+            this.toolStripLabel9.Text = "Other:";
+            // 
+            // pnlContent
+            // 
+            this.pnlContent.BackColor = System.Drawing.Color.Transparent;
+            this.pnlContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlContent.Location = new System.Drawing.Point(0, 25);
+            this.pnlContent.Name = "pnlContent";
+            this.pnlContent.Size = new System.Drawing.Size(435, 222);
+            this.pnlContent.TabIndex = 19;
+            // 
+            // btnChooseContinent
+            // 
+            this.btnChooseContinent.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnChooseContinent.Image = ((System.Drawing.Image)(resources.GetObject("btnChooseContinent.Image")));
+            this.btnChooseContinent.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnChooseContinent.Name = "btnChooseContinent";
+            this.btnChooseContinent.Size = new System.Drawing.Size(23, 22);
+            this.btnChooseContinent.Text = "??";
+            this.btnChooseContinent.Click += new System.EventHandler(this.btnChooseContinent_Click);
             // 
             // ContentLouContinent
             // 
@@ -37,14 +97,28 @@
             this.BackColor = System.Drawing.Color.White;
             this.BackgroundImage = global::LouMapInfoApp.Properties.Resources.LordOfUltima;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.Controls.Add(this.pnlContent);
+            this.Controls.Add(this.tbReportContinentOverview);
             this.DoubleBuffered = true;
             this.Name = "ContentLouContinent";
-            this.Size = new System.Drawing.Size(325, 247);
+            this.Size = new System.Drawing.Size(435, 247);
+            this.tbReportContinentOverview.ResumeLayout(false);
+            this.tbReportContinentOverview.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ToolStrip tbReportContinentOverview;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel8;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator4;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel9;
+        private System.Windows.Forms.Panel pnlContent;
+        private System.Windows.Forms.ToolStripButton btnChooseContinent;
+
+
 
     }
 }
--- LouMapInfo/LouMapInfoApp/V4/LouOfficial/ContentLouContinent.cs
+++ LouMapInfo/LouMapInfoApp/V4/LouOfficial/ContentLouContinent.cs
@@ -6,6 +6,9 @@ using System.Data;
 using System.Text;
 using System.Windows.Forms;
 using LouMapInfo.OfficialLOU.Entities;
+using LouMapInfo.Reports.core;
+using LouMapInfo.Reports.OfficialLOU;
+using System.Threading;
 
 namespace LouMapInfoApp.V4.LouOfficial
 {
@@ -20,11 +23,105 @@ namespace LouMapInfoApp.V4.LouOfficial
         public ContentLoUOfficial Frame
         {
             get { return m_Frame; }
-            set { m_Frame = value; }
+            set
+            {
+                m_Frame = value;
+                int ind = 1;
+                foreach (int ic in Session.World.Alliance(Session.AllianceID).ActiveContinents)
+                {
+                    ToolStripButton btnContinent = new ToolStripButton(ic.ToString("00"));
+                    btnContinent.Click += new EventHandler(btnContinent_Click);
+                    tbReportContinentOverview.Items.Insert(ind, btnContinent);
+                    ind++;
+                }
+            }
         }
         public ContentLouContinent()
         {
             InitializeComponent();
+        }
+
+        private void btnChooseContinent_Click(object sender, EventArgs e)
+        {
+            ContinentPicker cp = new ContinentPicker();
+            cp.ShowDialog();
+            if (cp.Continent >= 0)
+            {
+                btnChooseContinent.Text = "C" + cp.Continent.ToString("00");
+                OpenContinentReport(cp.Continent);
+            }
+        }
+
+        void btnContinent_Click(object sender, EventArgs e)
+        {
+            OpenContinentReport(int.Parse(((ToolStripButton)sender).Text));
+        }
+
+        private void OpenContinentReport(int c)
+        {
+            ContentEnabling(false);
+            new Thread(new ParameterizedThreadStart(OpenContinentReportAsync)).Start(c);
+        }
+
+        private void OpenContinentReportAsync(object o)
+        {
+            if (o is int)
+            {
+                LoUContinentInfo c = Session.World.Continent((int)o);
+                if (c != null)
+                {
+                    LoUContinentOverviewReport rep = new LoUContinentOverviewReport(c, OldLoUCityType.CityCastlePalace);
+                    rep.LoadIfNeeded();
+                    OpenReport(rep, 4);
+                }
+            }
+            ContentEnabling(true);
+
+            LoUPlayerInfo p = null;
+            if (o is int)
+                p = Session.World.Player((int)o);
+            else if (o is string)
+                p = Session.World.Player((string)o);
+
+            if (p == null)
+                ContentEnabling(true);
+            else
+            {
+                LoUPlayerOverviewReport rep = new LoUPlayerOverviewReport(p, OldLoUCityType.CityCastlePalace);
+                rep.LoadIfNeeded();
+                OpenReport(rep, 4);
+                ContentEnabling(true);
+            }
+        }
+        delegate void BoolHandler(bool isConnected);
+        public void ContentEnabling(bool value)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new BoolHandler(ContentEnabling), value);
+                return;
+            }
+            tbReportContinentOverview.Enabled = value;
+            if (value)
+                Frame.StopWaiting();
+            else
+            {
+                pnlContent.Controls.Clear();
+                Frame.StartWaiting();
+            }
+        }
+        public delegate void ReportHandler(ReportInfo r, int lvl);
+        public void OpenReport(ReportInfo r, int lvl)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new ReportHandler(OpenReport), r, lvl);
+                return;
+            }
+            // ReportForm.ShowReport(r, lvl);
+            ContentReport content = new ContentReport(r, lvl);
+            pnlContent.Controls.Add(content);
+            content.Dock = DockStyle.Fill;
         }
     }
 }
