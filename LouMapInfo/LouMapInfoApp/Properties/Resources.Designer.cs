dc8b0b1737e8a0e9b758529e17fd751c3b8650c9
Éric Massé <Ericmas001@users.noreply.github.com>
2011-03-27 17:52:44 +0000

    oops ! Too much cleaning :p

--- LouMapInfo/LouMapInfoApp/Properties/Resources.Designer.cs
+++ LouMapInfo/LouMapInfoApp/Properties/Resources.Designer.cs
@@ -284,6 +284,13 @@ namespace LouMapInfoApp.Properties {
             }
         }
         
+        internal static System.Drawing.Bitmap icon_help {
+            get {
+                object obj = ResourceManager.GetObject("icon_help", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
         internal static System.Drawing.Bitmap icon_recruit_slots {
             get {
                 object obj = ResourceManager.GetObject("icon_recruit_slots", resourceCulture);
