abd08926a73bbc8c08dfdbc4c4edc371ecec426f
Éric Massé <Ericmas001@users.noreply.github.com>
2013-03-20 02:29:01 +0000

    New Worlds + New way of gtting them

--- LouMapInfo/LoUListServers/Program.cs
+++ LouMapInfo/LoUListServers/Program.cs
@@ -14,10 +14,6 @@ namespace LoUListServers
     {
         static void Main(string[] args)
         {
-            string main = "en";
-            string[] others = { "de", "fr", "it", "pl", "pt", "pt_BR", "uk", "ar", "cs", "da", "fi", "nl", "no", "ro", "sk", "sv", "ru", "es", "tr", "hu" };
-            Array.Sort(others);
-
             Console.WriteLine(" ==== Retrieve LoU Servers List !! ==== ");
             Console.Write("Mail: ");
             string mail = "ericmas001@gmail.com"; //Console.ReadLine();
@@ -26,23 +22,34 @@ namespace LoUListServers
             string pass = Console.ReadLine();
 
             CookieContainer cookies = SessionInfo.ConnectToLoU(mail, pass);
+            string session = SessionInfo.GetSessionID(cookies);
 
-            string s = GatheringUtility.GetPageSource("http://www.lordofultima.com/en/game", cookies);
-            string my = StringUtility.Extract(s, "<div class=\"my-games\">", "<div class=\"world-new\">");
-            int ind = -1;
+            string xml = GatheringUtility.GetPageSource("http://prodcdngame.lordofultima.com/Farm/service.svc/ajaxEndpoint/1/session/" + session + "/worlds", "application/xml ; charset=UTF-8", cookies, Encoding.UTF8);
+            xml = xml.Replace("</server>\n\t<server>", "</server>|<server>");
+            string[] servers = xml.Split('|');
             ServerList.LoadFromWeb = false;
-            ServerList.AllServers.AddRange(getList(main, null, cookies));
-            while ((ind = my.IndexOf("<li class=\"online menu_bubble\"", ind+1)) >= 0)
+            foreach (string sr in servers)
             {
-                string world = StringUtility.Extract(my, "class=\"online menu_bubble\"", "</li>", ind);
-                string name = StringUtility.Extract(world, "id=\"#", "\">");
-                string url = StringUtility.Extract(world, "action=\"http://", ".aspx");
+                string url = StringUtility.Extract(sr, "<serverURL>", "/serverURL>");
                 string srv = StringUtility.Extract(url, "prodgame", ".lordofultima.com");
                 string id = StringUtility.Extract(url, ".com/", "/index");
-                ServerList.AllServers.Add(new ServerInfo(int.Parse(id), name, int.Parse(srv)));
+
+                string name = StringUtility.Extract(sr, "<servername>", "</servername>").Trim().Replace("العالم","Arab World");
+
+                string lang = StringUtility.Extract(sr, "<region>", "</region>");
+
+                if (lang != "en")
+                    name = "(" + lang + ") " + name;
+                else
+                {
+                    name = "  " + name;
+                    if (name.Contains("Castle"))
+                        name = name.Substring(1);
+                }
+
+                ServerInfo si = new ServerInfo(int.Parse(id), name, int.Parse(srv));
+                ServerList.AllServers.Add(si);
             }
-            foreach (string l in others)
-                ServerList.AllServers.AddRange(getList(l, l, cookies));
 
             ServerList.PopulateDictionnaries();
             string[] names = new string[ServerList.AllServers.Count];
@@ -52,43 +59,11 @@ namespace LoUListServers
             StreamWriter sw = File.CreateText("worlds.txt");
             foreach (string w in names)
             {
-                string line = ServerList.ServersByName[w].Id + ";"+w.Trim() + ";" + ServerList.ServersByName[w].Prod;
+                string line = String.Format("{0};{1};{2}", ServerList.ServersByName[w].Id, w.Trim(), ServerList.ServersByName[w].Prod);
                 sw.WriteLine(line);
                 Console.WriteLine(line);
             }
             sw.Close();
-        }
-        public static List<ServerInfo> getList(string lang, string prefix, CookieContainer cookies)
-        {
-            List<ServerInfo> servers = new List<ServerInfo>();
-
-            string s = GatheringUtility.GetPageSource("http://www.lordofultima.com/"+ lang + "/game", cookies);
-            if (s != null)
-            {
-                s = StringUtility.Extract(s, "<div class=\"world-new\">", "<div id=\"startgame_new\"");
-
-                int ind = -1;
-                while ((ind = s.IndexOf("<option", ind + 1)) >= 0)
-                {
-                    string world = StringUtility.Extract(s, "<option", "/option>", ind);
-                    string name = StringUtility.Extract(world, ">", "<").Trim();
-                    if (prefix != null)
-                        name = "(" + prefix + ") " + name;
-                    else
-                    {
-                        name = "  " + name;
-                        if (name.Contains("Castle"))
-                            name = name.Substring(1);
-                    }
-                    if (world.Contains("class=\"low\""))
-                        name = name.Remove(name.LastIndexOf('(')).TrimEnd();
-                    string url = StringUtility.Extract(world, "http://", ".aspx");
-                    string srv = StringUtility.Extract(url, "prodgame", ".lordofultima.com");
-                    string id = StringUtility.Extract(url, ".com/", "/index");
-                    servers.Add(new ServerInfo(int.Parse(id), name, int.Parse(srv)));
-                }
-            }
-            return servers;
         }
     }
 }
