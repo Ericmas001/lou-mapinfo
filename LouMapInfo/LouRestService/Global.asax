46c9a91c6571ead13e110290ad25cca885cc41a7
Éric Massé <Ericmas001@users.noreply.github.com>
2013-04-17 21:08:20 +0000

    LouRestService

---
+++ LouMapInfo/LouRestService/Global.asax
@@ -0,0 +1,1 @@
+<%@ Application Codebehind="Global.asax.cs" Inherits="LouRestService.Global" Language="C#" %>
---
+++ LouMapInfo/LouRestService/Global.asax.cs
@@ -0,0 +1,25 @@
+﻿using LouRestService.Services;
+using System;
+using System.ServiceModel.Activation;
+using System.Web;
+using System.Web.Routing;
+
+namespace LouRestService
+{
+    public class Global : HttpApplication
+    {
+        private void Application_Start(object sender, EventArgs e)
+        {
+            RegisterRoutes();
+        }
+
+        private void RegisterRoutes()
+        {
+            // Old Services => Soon to be deleted
+            RouteTable.Routes.Add(new ServiceRoute("TimeService", new WebServiceHostFactory(), typeof(TimeService)));
+
+            // New Services
+            RouteTable.Routes.Add(new ServiceRoute("User", new WebServiceHostFactory(), typeof(UserService)));
+        }
+    }
+}
